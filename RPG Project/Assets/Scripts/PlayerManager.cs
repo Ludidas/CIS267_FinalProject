7e9b725bc0769c4700f54263dac7d26259dfce33
SuperFire92 <nickdyk21@gmail.com>
2023-11-14 22:33:03 -0500

    Altered PlayerManager; Animation Template
    Changed the player movement to move with more control, and only rotate the interaction zone. Also added a template for animations in the PlayerManager script.

--- RPG Project/Assets/Scripts/PlayerManager.cs
+++ RPG Project/Assets/Scripts/PlayerManager.cs
@@ -11,7 +11,7 @@ public class PlayerManager : MonoBehaviour
     private Rigidbody2D rb;
 
     [Header("Attributes")]
-    [SerializeField] private float slowdownSpeed;
+    [SerializeField] private float speed;
     [SerializeField] private float deadZone;
 
     [Header("References")]
@@ -23,24 +23,48 @@ public class PlayerManager : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
-    //Keep this organized. Update() should contain as few functions as possible.
+    //Keep this organized. Update() should contain as few functions as possible, and it should be obvious what the functions do.
     private void Update()
     {
-        movePlayer();
+        updatePlayer();
+        animatePlayer();
     }
 
     #region Movement
 
-    //Handles all of the player's movement and rotation
+    //updatePlayer() does three things. Moves the player, rotates the player, and moves the camera. Each of these actions can be found in their respective functions.
+    public void updatePlayer()
+    {
+        movePlayer();
+        rotatePlayer();
+        moveCamera();
+    }
+
+    //Handles all of the player's movement
     public void movePlayer()
     {
-        //Gives the player speed in the direction they are moving
-        rb.velocity = (rb.velocity + playerMovement) / slowdownSpeed;
-        //Rotates the player in the direction they want to face. deadzone() ensure the player keeps facing their direction when they release the joystick
+        //Moves the player in their requested direction
+        rb.velocity = playerMovement * speed;
+    }
+
+    //Handles all of the player's rotations
+    public void rotatePlayer()
+    {
+        //Rotates the player's interactionZone in the direction they want to face. deadzone() ensure the player keeps facing their direction when they release the joystick
         if (deadzone(playerRotation))
         {
-            transform.rotation = Quaternion.LookRotation(Vector3.forward, (Vector3.up * playerRotation.x + Vector3.left * playerRotation.y));
+            interactionZone.transform.rotation = Quaternion.LookRotation(Vector3.forward, (Vector3.up * playerRotation.x + Vector3.left * playerRotation.y));
         }
+        //Rotates the interactionZone of the player if the user is not using the rotate joystick
+        else if (deadzone(playerMovement))
+        {
+            interactionZone.transform.rotation = Quaternion.LookRotation(Vector3.forward, (Vector3.up * playerMovement.x + Vector3.left * playerMovement.y));
+        }
+    }
+
+    //Handles the movement of the camera
+    public void moveCamera()
+    {
         //Forces the camera to follow the player without having to parent it.
         camera.transform.position = new Vector3(transform.position.x, transform.position.y, -10f);
     }
@@ -53,6 +77,66 @@ public class PlayerManager : MonoBehaviour
             return false;
         }
         return true;
+    }
+
+    #endregion
+
+    #region Animations
+
+    #warning THIS ANIMATION CODE DOES NOT WORK YET
+
+    //This will handle walking animations and directional animations
+    public void animatePlayer()
+    {
+        float rotation = Quaternion.LookRotation(Vector3.forward, (Vector3.up * playerMovement.x + Vector3.left * playerMovement.y)).eulerAngles.z;
+        if (rb.velocity != Vector2.zero)
+        {
+            //The player in here is walking. We'll need to use walk animations
+            if ((rotation >= 0 && rotation <= 45) || (rotation <= 360 && rotation >= 315))
+            {
+                //Player is facing right. Use right walking animations
+                Debug.Log("Walking Right");
+            }
+            else if (rotation >= 45 && rotation <= 135)
+            {
+                //Player is facing up. Use upwards walking animations
+                Debug.Log("Walking Up");
+            }
+            else if (rotation >= 135 && rotation <= 225)
+            {
+                //Player is facing left. Use left walking animations
+                Debug.Log("Walking Left");
+            }
+            else if (rotation >= 225 && rotation <= 315)
+            {
+                //Player is facing down. Use downwards walking animations
+                Debug.Log("Walking Down");
+            }
+        }
+        else
+        {
+            //The player in here is standing still. We'll need to use idle animations
+            if ((rotation >= 0 && rotation <= 45) || (rotation <= 360 && rotation >= 315))
+            {
+                //Player is facing right. Use right idle animations
+                Debug.Log("Standing Right");
+            }
+            else if (rotation >= 45 && rotation <= 135)
+            {
+                //Player is facing up. Use upwards idle animations
+                Debug.Log("Standing Up");
+            }
+            else if (rotation >= 135 && rotation <= 225)
+            {
+                //Player is facing left. Use left idle animations
+                Debug.Log("Standing Left");
+            }
+            else if (rotation >= 225 && rotation <= 315)
+            {
+                //Player is facing down. Use downwards idle animations
+                Debug.Log("Standing Down");
+            }
+        }
     }
 
     #endregion
