a89bb7faf1d9bfa7b454a25e6268ee7b36c45581
clambrix2 <144628231+clambrix2@users.noreply.github.com>
2023-11-16 18:36:14 -0500

    iceblock done
    i have finish the iceblock. there are some improvement that need to be made.

--- RPG Project/Assets/Scripts/Iceblock.cs
+++ RPG Project/Assets/Scripts/Iceblock.cs
@@ -1,18 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Iceblock : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private float startposx;
+    private float startposy;
+    private bool yaxis;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb= GetComponent<Rigidbody2D>();  
+       startposx = transform.position.x;
+        startposy = transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+  
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+      
+        if(collision.gameObject.CompareTag("Wall"))
+        {
+           
+        }
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            if (collision.gameObject.transform.position.x > startposx && yaxis == false)
+            {
+                rb.constraints = RigidbodyConstraints2D.None;
+                rb.constraints = RigidbodyConstraints2D.FreezePositionY;
+                rb.velocity = Vector2.left;
+            }
+            else if (collision.gameObject.transform.position.x < startposx && yaxis == false)
+            {
+                rb.constraints = RigidbodyConstraints2D.None;
+                rb.constraints = RigidbodyConstraints2D.FreezePositionY;
+                rb.velocity = Vector2.right;
+            }
+            else if(collision.gameObject.transform.position.y > startposy && yaxis == true)
+            {
+                rb.constraints = RigidbodyConstraints2D.None;
+                rb.constraints = RigidbodyConstraints2D.FreezePositionX;
+                rb.velocity = Vector2.down;
+            }
+            else if(collision.gameObject.transform.position.y < startposy && yaxis == true)
+            {
+                rb.constraints = RigidbodyConstraints2D.None;
+                rb.constraints = RigidbodyConstraints2D.FreezePositionX;
+                rb.velocity = Vector2.up;
+            }
+        }
+       
+        
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            yaxis = true;
+        } 
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        yaxis= false;
     }
 }
